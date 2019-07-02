2640ef0a2108dbf23bf99c64ed269ddddbf56ad7
Georgios Konstantopoulos <me@gakonst.com>
2019-07-02 20:55:52 +0300

    add traits for ethereum transaction store and account
    an account can optionally be associated with an erc20
    each account will always be associated with 1 type of token
    there is some reuse here from IldcpAccount which we can improve

--- crates/interledger-settlement-engines/src/lib.rs
+++ crates/interledger-settlement-engines/src/lib.rs
@@ -1,11 +1,43 @@
+#![recursion_limit = "128"]
 #[macro_use]
 extern crate log;
 
 #[macro_use]
 extern crate tower_web;
 
+extern crate ethabi;
+
 mod eth_engine;
 mod helpers;
 
+use ethereum_tx_sign::web3::types::Address;
+use futures::Future;
+use interledger_service::Account;
+
 pub use self::eth_engine::EthereumSettlementEngine;
 pub use self::helpers::{make_tx, TxSigner};
+
+pub trait EthereumAccount: Account {
+    fn token_adddress(&self) -> Option<Address> {
+        None
+    }
+}
+
+pub trait EthereumStore {
+    type Account: EthereumAccount;
+
+    /// Saves the Ethereum address associated with this account
+    /// called when creating an account on the API
+    fn save_account_address(
+        &self,
+        account_id: <Self::Account as Account>::AccountId,
+        address: Address,
+        token_address: Option<Address>,
+    ) -> Box<dyn Future<Item = (), Error = ()> + Send>;
+
+    /// Loads the Ethereum address associated with this account
+    fn load_account_addresses(
+        &self,
+        account_id: Vec<<Self::Account as Account>::AccountId>,
+    ) -> Box<dyn Future<Item = Vec<(Address, Address)>, Error = ()> + Send>;
+}
